7df1fa8e06d8699ac71a827ed1f92e0dfa15c531
김상혁 <sangkun@gimsanghyeog-ui-MacBookAir.local>
2022-05-02 21:20:44 +0900

    feat : 고객 컴포넌트를 구조화한다

--- src/App.js
+++ src/App.js
@@ -1,10 +1,48 @@
 import "./App.css";
-import Customer from "./component/customer";
+import { Customer } from "./component/customer";
+
+const customers = [
+  {
+    id: 1,
+    image: "https://placeImg.com/64/64/1",
+    name: "Sangkun",
+    age: 23,
+    gender: "male",
+    career: "GnB",
+  },
+  {
+    id: 2,
+    image: "https://placeImg.com/64/64/2",
+    name: "Tay",
+    age: 30,
+    gender: "male",
+    career: "GnB",
+  },
+  {
+    id: 3,
+    image: "https://placeImg.com/64/64/3",
+    name: "Summer",
+    age: 32,
+    gender: "female",
+    career: "GnB",
+  },
+];
 
 function App() {
   return (
     <div className="gray-background">
-      <Customer name={"Sangkun"} age={23} gender={"male"} career={"GnB"} />
+      {customers.map((customers) => {
+        return (
+          <Customer
+            id={customers.id}
+            image={customers.image}
+            name={customers.name}
+            age={customers.age}
+            gender={customers.gender}
+            career={customers.career}
+          />
+        );
+      })}
     </div>
   );
 }

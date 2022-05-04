85aec6e32d5d232daaccd1ed9943b2efc6eda850
Sangkun-svg <gims4752@gmail.com>
2022-05-04 14:51:53 +0900

    feat : 유저 데이터 로빙 뷰 생성

--- src/App.js
+++ src/App.js
@@ -8,27 +8,34 @@ import {
   TableRow,
   TableCell,
   Paper,
+  CircularProgress,
 } from "@mui/material";
 import { Component } from "react";
 
 class App extends Component {
   state = {
     customers: "",
+    completed: 0,
   };
-
   componentDidMount() {
+    this.timer = setInterval(this.progress, 20);
     this.callApi()
       .then((res) => this.setState({ customers: res }))
       .catch((error) => {
         console.error(error);
       });
   }
+  progress = () => {
+    const { completed } = this.state;
+    this.setState({ completed: completed >= 170 ? 0 : completed + 2 });
+  };
+
   callApi = async () => {
     const res = await axios.get("/api/customers");
     return res.data;
   };
   render() {
-    const { customers } = this.state;
+    const { customers, completed } = this.state;
     return (
       <Paper>
         <Table>
@@ -42,20 +49,28 @@ class App extends Component {
             </TableRow>
           </TableHead>
           <TableBody>
-            {customers
-              ? customers?.map((customers) => {
-                  return (
-                    <Customer
-                      id={customers.id}
-                      image={customers.image}
-                      name={customers.name}
-                      age={customers.age}
-                      gender={customers.gender}
-                      career={customers.career}
-                    />
-                  );
-                })
-              : ""}
+            {customers ? (
+              customers?.map((customers) => {
+                return (
+                  <Customer
+                    id={customers.id}
+                    image={customers.image}
+                    name={customers.name}
+                    age={customers.age}
+                    gender={customers.gender}
+                    career={customers.career}
+                  />
+                );
+              })
+            ) : (
+              <>
+                <TableRow>
+                  <TableCell colSpan={6} align={"center"}>
+                    <CircularProgress variant="determinate" value={completed} />
+                  </TableCell>
+                </TableRow>
+              </>
+            )}
           </TableBody>
         </Table>
       </Paper>
